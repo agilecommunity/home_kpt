fc14ebc286506927fe3a37cb936594c7ef598e39
upinetree <t.matsumoto@everyleaf.com>
2015-11-15 15:47:01 +0900

    登録できる種別を制限した

--- app/models/memo.rb
+++ app/models/memo.rb
@@ -1,4 +1,8 @@
 class Memo < ActiveRecord::Base
+  extend Enumerize
+
   validates :kind, presence: true
   validates :content, presence: true, length: { maximum: 100 }
+
+  enumerize :kind, in: [:keep, :problem]
 end
